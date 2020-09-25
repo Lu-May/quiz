037e6fe1160ebca78b279a89163722afbd3c7105
Lu-May <1054998345@qq.com>
2020-09-25 17:28:58 +0800

    [luyuehuan] feat:order page style

--- front-end/src/components/Order.js
+++ front-end/src/components/Order.js
@@ -1,9 +1,18 @@
 import React, { Fragment } from "react";
+import "./Order.css"
 
 const Order = () => {
   return (
     <Fragment>
-      <p>Oeder</p>
+      <table className="tableLine">
+        <tr className="orderHead">
+          <td className="inTable">名字</td>
+          <td className="inTable">单价</td>
+          <td className="inTable">数量</td>
+          <td className="inTable">单位</td>
+          <td className="inTable">操作</td>
+        </tr>
+      </table>
     </Fragment>
   );
 };
