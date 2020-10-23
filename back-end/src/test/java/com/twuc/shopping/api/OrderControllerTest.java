362391fdc75e69cf36facc456fd7251dc6dd14f0
Lu-May <1054998345@qq.com>
2020-10-23 11:58:47 +0800

    [luyuehuan] feat:implement add order test

--- back-end/src/test/java/com/twuc/shopping/api/OrderControllerTest.java
+++ back-end/src/test/java/com/twuc/shopping/api/OrderControllerTest.java
@@ -1,5 +1,11 @@
 package com.twuc.shopping.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.twuc.shopping.dto.OrderDto;
+import com.twuc.shopping.dto.ProductDto;
+import com.twuc.shopping.repository.OrderRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -16,10 +23,40 @@ public class OrderControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    OrderRepository orderRepository;
+
+    private String json;
+    @BeforeEach
+    public void setUp() throws JsonProcessingException {
+        ProductDto productDto = ProductDto.builder()
+                .name("cole")
+                .id(0)
+                .price(3)
+                .unit("罐")
+                .imgUrl("D:\\PowerShell\\TWU\\quiz\\front-end\\src\\img\\cola.jpg")
+                .build();
+        OrderDto orderDto = OrderDto.builder()
+                .id(0)
+                .productId(productDto.getId())
+                .count(2)
+                .build();
+        orderRepository.save(orderDto);
+        ObjectMapper objectMapper = new ObjectMapper();
+        json = objectMapper.writeValueAsString(orderDto);
+    }
     @Test
     public void should_get_order_list() throws Exception {
         mockMvc.perform(get("/products")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void should_add_order() throws Exception {
+        mockMvc.perform(post("/order")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
     }
 }
