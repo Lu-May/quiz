ee8b50824293c8677e454cdabd9ddbf19cf29dc0
Lu-May <1054998345@qq.com>
2020-10-23 09:27:47 +0800

    [luyuehuan] feat:get products list

--- back-end/src/main/java/com/twuc/shopping/api/ProductController.java
+++ back-end/src/main/java/com/twuc/shopping/api/ProductController.java
@@ -1,8 +1,21 @@
 package com.twuc.shopping.api;
 
+import com.twuc.shopping.dto.ProductDto;
+import com.twuc.shopping.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
+    @Autowired
+    ProductRepository productRepository;
 
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductDto>> getProducts() {
+        return ResponseEntity.ok(productRepository.findAll());
+    }
 }
