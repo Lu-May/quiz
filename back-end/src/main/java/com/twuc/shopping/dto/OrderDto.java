c4f84eb10adc523bf69c5ff1184b4056c18ada59
Lu-May <1054998345@qq.com>
2020-10-23 10:39:59 +0800

    [luyuehuan] feat:add order dto

--- back-end/src/main/java/com/twuc/shopping/dto/OrderDto.java
+++ back-end/src/main/java/com/twuc/shopping/dto/OrderDto.java
@@ -1,4 +1,25 @@
 package com.twuc.shopping.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product_order")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderDto {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private int productId;
+    private int count;
 }
